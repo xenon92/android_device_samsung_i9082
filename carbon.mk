2a6b588d3be1307c5699560da23e8d674d6e3a68
Shubhang <shubhang92@gmail.com>
2014-01-27 00:58:16 +0530

    bootanimation: switch back to 480 zip

--- carbon.mk
+++ carbon.mk
@@ -24,14 +24,9 @@ OVERLAY_TARGET := pa_hdpi
 PRODUCT_LOCALES := en_US de_DE zh_CN zh_TW cs_CZ nl_BE nl_NL en_AU en_GB en_CA en_NZ en_SG fr_BE fr_CA fr_FR fr_CH de_AT de_LI de_CH it_IT it_CH ja_JP ko_KR pl_PL ru_RU es_ES ar_EG ar_IL bg_BG ca_ES hr_HR da_DK en_IN en_IE en_ZA fi_FI el_GR iw_IL hi_IN hu_HU in_ID lv_LV lt_LT nb_NO pt_BR pt_PT ro_RO sr_RS sk_SK sl_SI es_US sv_SE tl_PH th_TH tr_TR uk_UA vi_VN
 
 # Boot animation
-# Giving the actual target screen resolution will include 480.zip which is glitchy
-# at the moment. Hence forcefully including 1080.zip as bootanimation
+TARGET_SCREEN_HEIGHT := 800
+TARGET_SCREEN_WIDTH := 480
 
-# TARGET_SCREEN_HEIGHT := 800
-# TARGET_SCREEN_WIDTH := 480
-
-TARGET_SCREEN_HEIGHT := 1920
-TARGET_SCREEN_WIDTH := 1080
 
 # Inherit some common Carbon stuff.
 $(call inherit-product, vendor/carbon/config/common_phone.mk)
