ef3f1df04b2d8ba86671e5aa47c131ceb90d2622
Shubhang Rathore <shubhang92@gmail.com>
2014-12-20 17:53:45 +0530

    i9082: disable WiFi HAL

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -49,6 +49,7 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/i9082/bluetooth
 BOARD_BLUEDROID_VENDOR_CONF := device/samsung/i9082/libbt_vndcfg.txt
 
 # Connectivity - Wi-Fi
+BOARD_NO_WIFI_HAL := true
 BOARD_WPA_SUPPLICANT_DRIVER := NL80211
 WPA_SUPPLICANT_VERSION      := VER_0_8_X
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
